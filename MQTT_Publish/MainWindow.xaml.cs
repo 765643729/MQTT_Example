d76460fe29218fc3dd876a88cb3ccbc0929b5d55
765643729 <765643729@163.com>
2021-06-23 07:56:48 +0800

    加入MQTT库(临时)

--- MQTT_Publish/MainWindow.xaml.cs
+++ MQTT_Publish/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MQTT_Publish
 {
@@ -25,13 +28,28 @@ namespace MQTT_Publish
             InitializeComponent();
         }
 
+        MqttClient client;
+
         private void Button_StartService(object sender, RoutedEventArgs e)
         {
+            string MQTT_BROKER_ADDRESS = "";
+
             this.MessageText.Text += "StartService\n";
+
+            MqttClient client = new MqttClient(IPAddress.Parse(MQTT_BROKER_ADDRESS));
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            string value = "";
+
+            string strValue = Convert.ToString(value);
         }
 
         private void Button_TestPublish(object sender, RoutedEventArgs e)
         {
+            var strValue = "";
+
+            client.Publish("/home/temperature", Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
             this.MessageText.Text += "TestPublish\n";
         }
 
--- MQTT_Subscribe/MainWindow.xaml.cs
+++ MQTT_Subscribe/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MQTT_Subscribe
 {
@@ -27,7 +30,26 @@ namespace MQTT_Subscribe
 
         private void Button_Subscribe(object sender, RoutedEventArgs e)
         {
+            string MQTT_BROKER_ADDRESS = "";
+            // create client instance 
+            MqttClient client = new MqttClient(IPAddress.Parse(MQTT_BROKER_ADDRESS));
+
+            // register to message received 
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
             this.MessageText.Text += "Subscribe\n";
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // handle message received 
         }
     }
 }
